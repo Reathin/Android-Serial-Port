bd7dca734f3bf900da682ad2d0a5a2e4ecf0c0cb
Rair <rairmmd@qq.com>
2018-04-20 11:08:04 +0800

    update

--- app/src/main/java/com/rair/android_serial_port/Test.java
+++ app/src/main/java/com/rair/android_serial_port/Test.java
@@ -1,5 +1,12 @@
 package com.rair.android_serial_port;
 
+import android.util.Log;
+
+import com.rairmmd.serialport.ByteUtil;
+import com.rairmmd.serialport.OnDataReceiverListener;
+
+import android_serialport_api.MachineControl;
+
 /**
  * Created by Rair on 2017/9/25.
  * Email:rairmmd@gmail.com
@@ -7,4 +14,20 @@ package com.rair.android_serial_port;
  */
 
 public class Test {
+
+    public static void main(String... args) {
+        MachineControl machineControl = new MachineControl("/dev/ttys", 9600);
+        boolean openCOM = machineControl.openCOM();
+        if (openCOM) {
+            machineControl.setOnDataReceiverListener(new OnDataReceiverListener() {
+                @Override
+                public void onDataReceiver(byte[] buffer, int size) {
+                    Log.i("Rair", ByteUtil.hexBytesToString(buffer));
+                }
+            });
+            machineControl.sendCMD(new byte[0x00]);
+        } else {
+            Log.i("Rair", "打开串口失败");
+        }
+    }
 }
